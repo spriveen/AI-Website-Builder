1794c48f2d0a85705e4eb36580969e3232a05353
spriveen <rpeanuka@gmail.com>
2026-05-01 16:15:15 +0530

    update website models.js

---
+++ backend/models/websiteModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose"
+
+const messageSchema = new mongoose.Schema({
+    role:{
+        type:String,
+        enum:["user", "ai"],
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
+
+const websiteSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    title:{
+        type:String,
+        default:"United Website"
+    },
+    latestCode:{
+        type:String,
+        required:true
+    },
+ conversation:[messageSchema],
+ deployed:{type:Boolean, default:false},
+ deployedUrl:{type:String},
+ slug:{type:String, unique:true}
+
+
+},{timestamps:true})
+
+export const Website = mongoose.model("Website", websiteSchema)
